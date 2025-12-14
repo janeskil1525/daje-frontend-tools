426d2207db076cdc667ad1b333a4a1f351a8dc33
janeskil1525 <janeskil1525@gmail.com>
2025-12-14 16:45:14 +0100

    GUI cleanup, bug fixes

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,4 +1,4 @@
 // src/environments/version.ts
-export const version = "0.1.0-dev";
+export const version = "0.9.1-dev";
 export const buildDate = new Date().toISOString();
 export const commitHash = "development";
